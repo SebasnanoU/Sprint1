c4357f79bbde7dcce333afd81e2946a8beb19153
Sebastian Rodriguez <jsrodriguezp03@libertadores.edu.co>
2023-05-27 18:57:43 +0000

    clase 27/05/2023

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4o$e)j%1gpv_xv3**qgsq#0l2p3))b4rip$_1j2b=$4l56wt1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
